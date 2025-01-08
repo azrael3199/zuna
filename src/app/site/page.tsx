712de3ce616acf672a9879b79b3ec79cdec3a0cd
Chinmay Ghungurde <chinmayghungurde@gmail.com>
2025-01-08 23:50:49 +0530

    trivial fix

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -18,14 +18,14 @@ export default function Home() {
     <>
       <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
         {/* grid */}
-        <div className="absolute bottom-0 left-0 right-0 top-0 dark:bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+        <div className="absolute bottom-0 left-0 right-0 top-0 dark:bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] z-[-10]" />
         <p className="text-center ">Run you agency, in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">
             {env.APP_NAME}
           </h1>
         </div>
-        <div className="flex justify-center items-center relative md:mt-[-50px]">
+        <div className="flex justify-center items-center relative md:mt-[-70px]">
           <Image
             src="/assets/preview.png"
             alt="Banner Image"
@@ -36,7 +36,7 @@ export default function Home() {
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10" />
         </div>
       </section>
-      <section className="flex justify-center items-center flex-col gap-4 md:mt-20">
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px] pb-10">
         <h2 className="text-4xl text-center">Choose what fits you right</h2>
         <p className="text-muted-foreground text-center">
           Our straightforward pricing plans are tailored to meet your needs. If
